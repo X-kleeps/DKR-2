d6f48e5ee321b2ae313b4129f95b60c53d25cb57
X-kleeps <117731041+X-kleeps@users.noreply.github.com>
2022-11-08 14:38:56 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,26 @@
+﻿var
+  a: string; sum: integer;
+  mas: array[1..10] of integer;
+
+begin
+  var s:= readlninteger;
+  for var k := 1 to 5 do
+  begin
+    mas[k] := random(1, 10);
+    print(' ', mas[k]);
+  end;
+  for var k := 1 to 5 do
+  begin
+    if (sum < s) then
+    begin
+      sum := mas[k] + sum;
+      if (k = 5) and (sum < s) then
+        println(' Суммы нет');
+    end
+    else 
+     begin 
+      println(' Итог=', k - 1);
+      break;
+     end;
+  end;
+end.
---
+++ 2.pas
@@ -0,0 +1,30 @@
+﻿var s1,s2:string; ds,ds2,i: integer; f: boolean;
+begin
+  var s1:= readlnstring;
+  var s2:= readlnstring;
+  i:= 1;
+  ds:= Length(s1);
+  ds2:= Length(s2);
+  f:= false;
+  while i <= ds - ds2 + 1 do
+  begin
+    if (s1[i]=s2[1]) then
+    begin
+      f:= true;
+      for var j := 1 to ds2 - 1 do
+        if (s1[i + j]<>s2[j + 1]) then
+        begin
+          i:= i + j;
+          f:= false;
+          break
+        end;
+      if f then
+      begin
+        print('является');
+        exit;
+      end;
+    end;
+    i:=i+ 1;
+  end;
+  print('не является');
+end.
