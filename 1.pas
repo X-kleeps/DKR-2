8c2f55dce6c4252c6fe6b2ceb82e47604a7cc0ea
X-kleeps <117731041+X-kleeps@users.noreply.github.com>
2022-11-08 14:59:13 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,29 @@
+﻿var
+  a: string; sum: integer;
+  mas: array[1..9999] of integer;
+begin
+  print('Введите предел массива');
+  var g:=readinteger;
+  for var k := 1 to g do
+  begin
+    mas[k] := random(1, 10);
+    print(mas[k]);
+  end;
+  println;
+  print('Введите число');
+  var s:= readlninteger;
+  for var k := 1 to g do
+  begin
+    if (sum < s) then
+    begin
+      sum := mas[k] + sum;
+      if (k = g) and (sum < s) then
+        println(' Суммы нет');
+    end
+    else 
+     begin 
+      println(' Итог=', k - 1);
+      break;
+     end;
+  end;
+end.
